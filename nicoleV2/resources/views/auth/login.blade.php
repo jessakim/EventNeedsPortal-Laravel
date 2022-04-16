386566b10fb6910c9b65053fc88590da4190adbf
DESKTOP-G0KHUU1\JRemonte <june.remonte@taxsystems.com>
2022-04-16 20:28:39 +0800

    login page some modifications

--- nicoleV2/resources/views/auth/login.blade.php
+++ nicoleV2/resources/views/auth/login.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 <div class="container">
+    <h3>Login here!</h3><h5><b><i>weHunt</i></b> will assist on your needs...</h5>
+    <hr>
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -66,7 +66,6 @@
                         </div>
                     </form>
                 </div>
-            </div>
         </div>
     </div>
 </div>
